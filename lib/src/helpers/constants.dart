55bd0aadb6ce9fc2c46baf135097a99a3f8b5dc6
vijayrajuyj1 <134908434+vijayrajuyj1@users.noreply.github.com>
2024-09-04 11:39:42 +0530

    Update constants.dart

--- lib/src/helpers/constants.dart
+++ lib/src/helpers/constants.dart
@@ -1,5 +1,5 @@
 class Constant {
-  static const String newsApiKey = 'cab817200f92426bacb4edd2373e82ef';
+  static const String newsApiKey = 'https://newsapi.org/v2/everything?q=tesla&from=2024-08-04&sortBy=publishedAt&apiKey=API_KEY';
   static const String baseUrl = 'https://newsapi.org/v2/';
   static const String topHeadLine = '/top-headlines';
 }
